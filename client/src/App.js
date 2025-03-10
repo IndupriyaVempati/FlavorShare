8df7c76ca2dd13ecdbea8b3b2bc325599d4409f4
priyamandot <mandotpriya04@gmail.com>
2025-03-10 22:18:21 +0530

    Chatbot integrated

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Home from "./components/Home"; // Just render the Home component
+import Chatbot from "./components/Chatbot"
 
 const App = () => {
-  return <Home />;
+  return(
+  <>
+    <Home />
+    <Chatbot />
+  </>);
 };
 
 export default App;
